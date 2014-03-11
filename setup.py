83e6bbf7eb566d54535f91d7303210b0183f8f13
Wes Kendall <wes.kendall@tryambition.com>
2014-03-10 23:40:10 -0400

    added base project files to template

---
+++ setup.py
@@ -0,0 +1,46 @@
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215_
+import multiprocessing
+assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = '{{ app_name }}/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+setup(
+    name='{{ project_name }}',
+    version=get_version(),
+    description='{{ project_description }}',
+    long_description=open('README.md').read(),
+    url='http://github.com/ambitioninc/{{ project_name }}/',
+    author='{{ author_name }}',
+    author_email='{{ author_email }}',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        'django>=1.4',
+    ],
+    tests_require=[
+        'psycopg2',
+        'django-nose',
+        'south',
+    ],
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+)
