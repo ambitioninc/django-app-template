f96d614b3bcc288a7b17e92d7afbd5951ae0791c
Wes Kendall <wesleykendall@gmail.com>
2014-04-27 12:29:48 -0400

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from settings import configure_settings
+
+
+if __name__ == '__main__':
+    configure_settings()
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
