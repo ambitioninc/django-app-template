7eb83e9f8c4196b75fc12ed864fb177e5a915db6
Wes Kendall <wesleykendall@gmail.com>
2014-04-27 12:29:13 -0400

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,53 +1,17 @@
 """
 Provides the ability to run test on a standalone Django app.
 """
-import os
 import sys
 from django.conf import settings
 from optparse import OptionParser
 
+from django.conf import settings
 
-if not settings.configured:
-    # Determine the database settings depending on if a test_db var is set in CI mode or not
-    test_db = os.environ.get('DB', None)
-    if test_db is None:
-        db_config = {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ambition_dev',
-            'USER': 'ambition_dev',
-            'PASSWORD': 'ambition_dev',
-            'HOST': 'localhost'
-        }
-    elif test_db == 'postgres':
-        db_config = {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'USER': 'postgres',
-            'NAME': '{{ project_name }}',
-        }
-    elif test_db == 'sqlite':
-        db_config = {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '{{ project_name }}',
-        }
-    else:
-        raise RuntimeError('Unsupported test DB {0}'.format(test_db))
+from settings import configure_settings
 
-    settings.configure(
-        DATABASES={
-            'default': db_config,
-        },
-        INSTALLED_APPS=(
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.admin',
-            'south',
-            '{{ project_name }}',
-            '{{ project_name }}.tests',
-        ),
-        ROOT_URLCONF='{{ project_name }}.urls',
-        DEBUG=False,
-    )
+
+# Configure the default settings
+configure_settings()
 
 # Django nose must be imported here since it depends on the settings being configured
 from django_nose import NoseTestSuiteRunner
